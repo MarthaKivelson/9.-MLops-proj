fcb182a32cbba08c6c7f6c7ef79760bd01edb46a
Uttam K P <95496600+SlimShadyAnonymous@users.noreply.github.com>
2025-07-30 11:14:07 +0530

    project structure ready

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+from setuptools import find_packages
+
+setup(
+    name='src',
+    version='0.0.1',
+    author='Uttam K P',
+    author_email="kputtam7@gmail.com",
+    packages=find_packages()
+)
