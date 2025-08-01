02227e674aa80361f53d96ea98e58fc22beac0bc
Uttam K P <95496600+SlimShadyAnonymous@users.noreply.github.com>
2025-08-01 22:43:14 +0530

    data transformation half done

--- src/constants/__init__.py
+++ src/constants/__init__.py
@@ -15,6 +15,7 @@ MODEL_FILE_NAME = "model.pkl"
 FILE_NAME: str = "data.csv"
 TRAIN_FILE_NAME: str = "train.csv"
 TEST_FILE_NAME: str = "test.csv"
+SCHEMA_FILE_PATH: str = os.path.join("config", "schema.yaml")
 
 
 #Data ingestion related constants start with DATA_INGESTION varname
@@ -23,3 +24,13 @@ DATA_INGESTION_DIR_NAME: str = "data_ingestion"
 DATA_INGESTION_FEATURE_STORE_DIR: str = "feature_store"
 DATA_INGESTION_INGESTED_DIR: str = "ingested"
 DATA_INGESTION_TRAIN_TEST_SPLIT_RATIO: float = 0.25
+
+#Data validation
+DATA_VALIDATION_DIR_NAME: str = "data_validation"
+DATA_VALIDATION_FILE_NAME: str = "report.yaml"
+
+#Data transformation
+DATA_TRANSFORMATION_DIR_NAME: str = "data_transformation"
+DATA_TRANSFORMATION_TRANSFORMED_DIR: str = "transformed"
+DATA_TRANSFORMATION_TRANSFORMED_OBJECT_DIR: str = "transformed_object"
+PREPROCSSING_OBJECT_FILE_NAME: str = "model.pkl" 
