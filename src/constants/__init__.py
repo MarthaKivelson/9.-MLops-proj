38ec930e5be89a94c6b0d3783d53763a25eb4383
Uttam K P <95496600+SlimShadyAnonymous@users.noreply.github.com>
2025-08-02 12:32:48 +0530

    data transformation done

--- src/constants/__init__.py
+++ src/constants/__init__.py
@@ -33,4 +33,7 @@ DATA_VALIDATION_FILE_NAME: str = "report.yaml"
 DATA_TRANSFORMATION_DIR_NAME: str = "data_transformation"
 DATA_TRANSFORMATION_TRANSFORMED_DIR: str = "transformed"
 DATA_TRANSFORMATION_TRANSFORMED_OBJECT_DIR: str = "transformed_object"
-PREPROCSSING_OBJECT_FILE_NAME: str = "model.pkl" 
+
+TARGET_COLUMN = "Response"
+CURRENT_YEAR = date.today().year
+PREPROCSSING_OBJECT_FILE_NAME = "preprocessing.pkl" 
