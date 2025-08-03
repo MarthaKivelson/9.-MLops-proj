79a11ab0809f1e74d222ab29e77b3bde54d9562f
Uttam K P <95496600+SlimShadyAnonymous@users.noreply.github.com>
2025-08-03 15:32:43 +0530

    ready till model pusher

--- src/entity/config_entity.py
+++ src/entity/config_entity.py
@@ -48,3 +48,14 @@ class ModelTrainingConfig:
     _max_depth = MIN_SAMPLES_SPLIT_MAX_DEPTH
     _criterion = MIN_SAMPLES_SPLIT_CRITERION
     _randon_state = MIN_SAMPLES_SPLIT_RANDOM_STATE 
+
+@dataclass 
+class ModelEvaluationConfig:
+    changed_treshold_score: float = MODEL_EVALUATION_CHANGED_THRESHOLD_SCORE
+    bucket_name: str = MODEL_BUCKET_NAME
+    s3_model_key_path: str = MODEL_FILE_NAME
+
+@dataclass
+class ModelPusherConfig:
+    bucket_name: str = MODEL_BUCKET_NAME
+    s3_model_key_path: str = MODEL_FILE_NAME
